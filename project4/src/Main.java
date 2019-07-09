a3e20c6778c37ad25f917d3d15f121c508e4e8f0
jmcs811 <justincaseyy69@gmail.com>
2019-07-09 19:25:43 -0400

    Added most of the required functionality.

--- project4/src/Main.java
+++ project4/src/Main.java
@@ -1,6 +1,209 @@
-public class Main {
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Main extends JFrame {
+
+  private String id;
+  private String name;
+  private String major;
+  private String selection;
+  HashMap<String, Student> studentDB = new HashMap<>();
+
+  private Main() {
+    super("Project 4");
+
+    JPanel main = new JPanel();
+
+    main.setLayout(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+
+    // TextFields
+    JTextField idTextField = new JTextField();
+    JTextField nameTextField = new JTextField();
+    JTextField majorTextField = new JTextField();
+
+    // labels
+    JLabel idLabel = new JLabel("ID: ");
+    JLabel nameLabel = new JLabel("NAME: ");
+    JLabel majorLabel = new JLabel("MAJOR: ");
+    JLabel selectLabel = new JLabel("PICK SELECTION: ");
+
+    JButton processButton = new JButton("Process Request");
+
+    c.fill = GridBagConstraints.HORIZONTAL;
+    String[] selectOptions = {"Insert", "Delete", "Find", "Update"};
+    JComboBox<String> selectCombobox = new JComboBox<String>(selectOptions);
+
+    // Add idLabel
+    c.insets = new Insets(0, 0, 5, 5);
+    c.gridx = 0;
+    c.gridy = 0;
+    main.add(idLabel, c);
+
+    // Add idTxt
+    c.insets = new Insets(0, 5, 5, 0);
+    c.gridx = 1;
+    c.gridy = 0;
+    main.add(idTextField, c);
+
+    // Add nameLabel
+    c.insets = new Insets(5, 0, 5, 5);
+    c.gridx = 0;
+    c.gridy = 1;
+    main.add(nameLabel, c);
+
+    // Add nameTxt
+    c.insets = new Insets(5, 5, 5, 0);
+    c.gridx = 1;
+    c.gridy = 1;
+    main.add(nameTextField, c);
+
+    // Add majorLabel
+    c.insets = new Insets(5, 0, 5, 5);
+    c.gridx = 0;
+    c.gridy = 2;
+    main.add(majorLabel, c);
+
+    // Add majorTxt
+    c.insets = new Insets(5, 5, 5, 0);
+    c.gridx = 1;
+    c.gridy = 2;
+    main.add(majorTextField, c);
+
+    // Add selectLabel
+    c.insets = new Insets(5, 0, 5, 5);
+    c.gridx = 0;
+    c.gridy = 3;
+    main.add(selectLabel, c);
+
+    // Add selectCombo
+    c.insets = new Insets(5, 5, 5, 0);
+    c.gridx = 1;
+    c.gridy = 3;
+    main.add(selectCombobox, c);
+
+    // Add processBtn
+    c.insets = new Insets(5, 0, 0, 5);
+    c.gridx = 0;
+    c.gridy = 4;
+    main.add(processButton, c);
+
+    // Add Main Panel
+    add(main);
+
+    processButton.addActionListener((ActionEvent e) -> {
+      id = idTextField.getText();
+      name = nameTextField.getText();
+      major = majorTextField.getText();
+      selection = selectCombobox.getSelectedItem().toString();
+
+      try {
+        if (id.isEmpty()) {
+          throw new NullPointerException();
+        } else {
+          switch (selection) {
+            case "Insert":
+              if (studentDB.containsKey(id)) {
+                JOptionPane.showMessageDialog(null,
+                    "Student already exists!");
+              } else {
+                studentDB.put(id, new Student(name, major));
+                JOptionPane.showMessageDialog(null,
+                    name + " added to the the database");
+              }
+              break;
+            case "Delete":
+              if (!studentDB.containsKey(id)) {
+                JOptionPane.showMessageDialog(null,
+                    "Student does not exist");
+              } else {
+                studentDB.remove(id);
+                JOptionPane.showMessageDialog(null,
+                    name + " removed from the database");
+              }
+              break;
+
+            case "Find":
+              if (!studentDB.containsKey(id)) {
+                JOptionPane.showMessageDialog(null,
+                    "ID does not exits",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+              } else {
+                studentDB.get(id);
+                String student = studentDB.get(id).toString();
+                JOptionPane.showMessageDialog(null,
+                    "Student Found!\n" + student,
+                    "Success",
+                    JOptionPane.INFORMATION_MESSAGE);
+              }
+              break;
+
+            case "Update":
+              if (studentDB.containsKey(id)) {
+                String grade = (String) JOptionPane.showInputDialog(null,
+                    "Choose Grade: ", "", JOptionPane.QUESTION_MESSAGE);
+                if (grade != null) {
+                  String creditHours = (String) JOptionPane.showInputDialog(null,
+                      "Choose Credits: ", "", JOptionPane.QUESTION_MESSAGE);
+                  if (creditHours != null) {
+                    studentDB.get(id).courseCompleted(grade, Integer.parseInt(creditHours));
+                    JOptionPane.showMessageDialog(null,
+                        "Student info updated");
+                  } else {
+                    JOptionPane.showMessageDialog(null,
+                        "credits value is empty",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                  }
+                } else {
+                  JOptionPane.showMessageDialog(null,
+                      "Grade value is empty",
+                      "Error",
+                      JOptionPane.ERROR_MESSAGE);
+                }
+              } else {
+                JOptionPane.showMessageDialog(null,
+                    "ID does not exits",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+              }
+              break;
+          }
+        }
+      } catch (NullPointerException npe) {
+        JOptionPane.showMessageDialog(null,
+            "The id field is empty",
+            "Error",
+            JOptionPane.ERROR_MESSAGE);
+      }
+
+      idTextField.setText("");
+      nameTextField.setText("");
+      majorTextField.setText("");
+    });
+
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setSize(350, 200);
+    setLocationRelativeTo(null);
+    setResizable(false);
+    setVisible(true);
+
+  }
+
+  public static void main(String[] args) {
+
+    new Main();
+
+  }
 }
--- project4/src/Student.java
+++ project4/src/Student.java
@@ -1,5 +1,45 @@
 public class Student {
 
+  String name;
+  String major;
+  double gpa;
+  int credits;
+  int gradePoints;
+  int qualitPoints;
 
+  public Student(String name, String major) {
+    this.name = name;
+    this.major = major;
+    this.credits = 0;
+    this.qualitPoints = 0;
+  }
 
+  public void courseCompleted(String grade, int creditHours) {
+    grade = grade.toUpperCase();
+    switch (grade) {
+      case "A":
+        gradePoints = 4;
+        break;
+      case "B":
+        gradePoints = 3;
+        break;
+      case "C":
+        gradePoints = 2;
+        break;
+      case "D":
+        gradePoints = 1;
+        break;
+    }
+
+    gradePoints = gradePoints * creditHours;
+    qualitPoints += gradePoints;
+    credits += creditHours;
+
+    gpa = qualitPoints / credits;
+  }
+
+  @Override
+  public String toString() {
+    return "Name: " + name + "\nMajor: " + major + "\nGPA: " + gpa + "\n";
+  }
 }
